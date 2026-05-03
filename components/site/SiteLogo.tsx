f46d886d3afbf58d6c6eb1a425895a7bc514d6ae
David Wolfe <dwolfe66@hotmail.com>
2026-05-03 16:52:07 -0700

    Revert "Replace site logo with DW asset"
    This reverts commit 4301d8dc48d224acf059fda928018e2372491833.

--- components/site/SiteLogo.tsx
+++ components/site/SiteLogo.tsx
@@ -1,16 +1,10 @@
-import Image from "next/image";
-
 export function SiteLogo() {
   return (
     <span className="siteLogo" aria-label="David Wolfe">
-      <Image
-        className="siteLogoImage"
-        src="/logo/dwlogo.png"
-        alt=""
-        width={44}
-        height={44}
-        priority
-      />
+      <span className="siteLogoMark" aria-hidden="true">
+        <span>D</span>
+        <span>W</span>
+      </span>
       <span className="siteLogoWord">
         David Wolfe
       </span>
